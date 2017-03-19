776826edd4104e40b2ba77a743e7dbd69f05793e
Miguel <m.auso.p@gmail.com>
2017-03-19 11:13:16 +0100

    Reorganize folders

---
+++ RPI-arm/squid3/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+SQUID_LOG=/var/log/squid3
+SQUID_CACHE=/var/spool/squid3
+SQUID_USER=proxy
+SQUID_CONF=/etc/squid/squid.conf
+
+mkdir -p ${SQUID_LOG}
+chmod -R 755 ${SQUID_LOG}
+chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG}
+#chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CONF}
+
+mkdir -p ${SQUID_CACHE}
+chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE}
+
+if [[ ! -d ${SQUID_CACHE}/00 ]]; then
+    echo "Initializing cache..."
+    squid3 -N -z
+fi
+
+echo "Starting squid3..."
+exec squid3 -NYCd 1
